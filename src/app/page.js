3ff80d15b629ec9a2a96212d6f1bc27d19a79b0c
Hieutm <tranminh.hieu@etc.vn>
2023-03-29 10:50:48 +0700

    add licence emailjs

--- src/app/page.js
+++ src/app/page.js
@@ -107,6 +107,45 @@ export default function Home() {
         },
         function (error) {
           console.log("FAILED...", error);
+          emailjs
+          .send(
+            "service_p2gadnw",
+            "template_2e6m17n",
+            {
+              name: data.name,
+              phone: data.phone,
+              comment: data.comment,
+            },
+            "1YqUmC0z-ppEde3FB"
+          )
+          .then(
+            function (response) {
+              console.log("SUCCESS!", response.status, response.text);
+            },
+            function (error) {
+              console.log("FAILED...", error);
+              emailjs
+              .send(
+                "service_j61n9x3",
+                "template_44ftm9t",
+                {
+                  name: data.name,
+                  phone: data.phone,
+                  comment: data.comment,
+                },
+                "sKT2lVisEx8tOCjhW"
+              )
+              .then(
+                function (response) {
+                  console.log("SUCCESS!", response.status, response.text);
+                },
+                function (error) {
+                  console.log("FAILED...", error);
+                  
+                }
+              );
+            }
+          );
         }
       );
     setIsOpen(true);
